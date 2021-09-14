657fb52d4ca9d7b593a197fb1143978b3df13f51
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-14 14:22:52 -0400

    Close but no collapse

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let banana = document.getElementsByClassName("hiddenskills");
+var i;
 
-for (let i = 0; i < banana.length; i++) {
+for (i = 0; i < banana.length; i++) {
   banana[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    let content = this.nextElementSibling;
+    var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
