f5a834a376cd61bd4646619d3e9ecbae66ff5ddc
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-14 15:01:25 -0400

    tests passed will finish other stuff later

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 let banana = document.getElementsByClassName("hiddenskills");
-var i;
 
-for (i = 0; i < banana.length; i++) {
+for (let i = 0; i < banana.length; i++) {
   banana[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
