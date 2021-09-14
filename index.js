9d0ad53fedeb108f914cf3cf35f880b07db8783c
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-14 13:49:51 -0400

    Collapsible not working yet

---
+++ index.js
@@ -0,0 +1,13 @@
+let banana = document.getElementsByClassName("hiddenskills");
+
+for (let i = 0; i < banana.length; i++) {
+  banana[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
